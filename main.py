4261d1608c8acbe0e2fe3113efd6501a4db9bc37
Mathieu Harb <m-harbus@hotmail.com>
2022-02-21 21:28:34 -0500

    forgot parenthese

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.casefold == "!bo3":
+    if message.content.casefold() == "!bo3":
         matches = series(3)
         embed = create_embed(matches, 3)
         await message.channel.send(embed=embed)
