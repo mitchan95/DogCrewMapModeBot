07a628adc7621c71148604e850e87a2a95ac486e
mitchan95 <131890828+mitchan95@users.noreply.github.com>
2024-10-31 15:02:17 +1000

    Update main.py

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ def coinflip():
 def rand_number():
     return random.randint(1, 10)
 
-COMMAND_LOG_COUNT = {'BO3_arena': 0, 'BO5_arena': 0, 'BO7_arena': 0, BO3_hcs': 0, 'BO5_hcs': 0, 'BO7_hcs': 0, 'Coinflip': 0, 'Number': 0}
+COMMAND_LOG_COUNT = {'BO3_arena': 0, 'BO5_arena': 0, 'BO7_arena': 0, 'BO3_hcs': 0, 'BO5_hcs': 0, 'BO7_hcs': 0, 'Coinflip': 0, 'Number': 0}
 
 def handle_bo_command(length, message):
     matches = series(length, OBJS, SLAYER)
