32103fbd3c5c2443dbcde8c841cabdc47c04d7b8
mitchan95 <131890828+mitchan95@users.noreply.github.com>
2024-11-10 18:21:03 +1000

    Update main (add legacy playlist).py

--- main.py
+++ main.py
@@ -20,17 +20,16 @@ token = os.environ.get("DISCORD_BOT_TOKEN")
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# arena Mode Objectives and Slayer maps
+# Arena Mode Objectives and Slayer maps
 arena_OBJS = {
-    "Capture the Flag": ["Aquarius", "Argyle", "Empyrean", "Forbidden", "Fortress", "Inquisitor"],
+    "Capture the Flag": ["Aquarius", "Empyrean", "Forbidden", "Fortress", "Inquisitor"],
     "Oddball": ["Live Fire", "Recharge", "Streets"],
     "Strongholds": ["Live Fire", "Recharge"],
     "King of the Hill": ["Live Fire", "Recharge", "Solitude"],
 }
-arena_SLAYER = ["Aquarius", "Live Fire", "Recharge", "Streets", "Solitude", "Fortress","Inquisitor"]
+arena_SLAYER = ["Aquarius", "Live Fire", "Recharge", "Streets", "Solitude", "Fortress", "Inquisitor"]
 
-# hcs Mode Objectives and Slayer maps
-
+# HCS Mode Objectives and Slayer maps
 hcs_OBJS = {
     "Capture the Flag": ["Aquarius", "Empyrean", "Forbidden"],
     "Oddball": ["Live Fire", "Recharge", "Streets"],
@@ -39,33 +38,43 @@ hcs_OBJS = {
 }
 hcs_SLAYER = ["Aquarius", "Live Fire", "Recharge", "Streets", "Solitude"]
 
+# Legacy Mode Objectives and Slayer maps
+legacy_OBJS = {
+    "Capture the Flag": ["Aquarius", "Argyle", "Empyrean", "Forbidden", "Fortress", "Inquisitor", "Catalyst", "Starboard"],
+    "Oddball": ["Live Fire", "Recharge", "Streets"],
+    "Strongholds": ["Live Fire", "Recharge", "Interference", "Solitude"],
+    "King of the Hill": ["Live Fire", "Recharge", "Solitude", "Interference"],
+}
+legacy_SLAYER = ["Aquarius", "Live Fire", "Recharge", "Streets", "Solitude", "Fortress", "Inquisitor", "Interference", "Starboard"]
+
 def pick_map(available_maps, picked_maps, last_n=2):
-    """
-    Pick a map ensuring it wasn't picked in the last_n matches
-    """
     valid_maps = list(set(available_maps) - set(picked_maps[-last_n:]))
     if valid_maps:
         return random.choice(valid_maps)
     return None
 
-def series(length, mode="arena"):
-    if mode == "arena":
+def series(length, mode="Arena"):
+    if mode == "Arena":
         gts = list(arena_OBJS)
         slayer_maps = copy.deepcopy(arena_SLAYER)
         temp_objs = copy.deepcopy(arena_OBJS)
-    elif mode == "hcs":
+    elif mode == "HCS":
         gts = list(hcs_OBJS)
         slayer_maps = copy.deepcopy(hcs_SLAYER)
         temp_objs = copy.deepcopy(hcs_OBJS)
+    elif mode == "Legacy":
+        gts = list(legacy_OBJS)
+        slayer_maps = copy.deepcopy(legacy_SLAYER)
+        temp_objs = copy.deepcopy(legacy_OBJS)
     else:
-        raise ValueError("Invalid mode. Choose 'arena' or 'hcs'.")
+        raise ValueError("Invalid mode. Choose 'Arena', 'HCS', or 'Legacy'.")
 
     picked_gt = []
     picked_maps = []
     games = []
 
     for i in range(length):
-        if i in [1, 4, 6]:  # Slayer rounds for certain matches
+        if i in [1, 4, 6]:
             cur_map = pick_map(slayer_maps, picked_maps, 2)
             if cur_map:
                 picked_maps.append(cur_map)
@@ -88,125 +97,59 @@ def series(length, mode="arena"):
                 games.append(f"{gt} - {cur_map}")
 
     return games
- 
+
 def create_embed(matches, length, mode):
-    # Adjust the title based on the mode
     embed = discord.Embed(
-        title=f"{mode} BO{length} Series",
+        title=f"{mode} Bo{length} Series",
         description=f"{mode} maps - best of {length}"
     )
-    
-    # Set the thumbnail URL
-    embed.set_thumbnail(
-        url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQBygbA4-Ry2hYeY4S4cYU1iljNRD7e2_1WQ&s"
-    )
-
-    # Add each match as a field in the embed
-    for i in range(len(matches)):
-        embed.add_field(name=f"Game {i + 1}", value=matches[i], inline=False)
-
+    embed.set_thumbnail(url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQBygbA4-Ry2hYeY4S4cYU1iljNRD7e2_1WQ&s")
+    for i, match in enumerate(matches):
+        embed.add_field(name=f"Game {i + 1}", value=match, inline=False)
     return embed
 
 def coinflip():
-    percent = random.randint(0, 100);
-    if percent < 50:
-        return "Heads"
-    elif percent > 50:
-        return "Tails"
-    else:
-        return "You're both losers!"
+    return "Heads" if random.randint(0, 100) < 50 else "Tails"
 
 def rand_number():
     return random.randint(1, 10)
 
-COMMAND_LOG_COUNT = {'bo3_arena': 0, 'bo5_arena': 0, 'bo7_arena': 0, 'bo3_hcs': 0, 'bo5_hcs': 0, 'bo7_hcs': 0, 'Coinflip': 0, 'Number': 0}
-
-def handle_bo_command(length, message):
-    matches = series(length, OBJS, SLAYER)
-    embed = create_embed(matches, length)
-    COMMAND_LOG_COUNT[f'BO{length}'] += 1
-    return embed
-
-COMMANDS = {
-    '!bo3_arena': lambda m: handle_bo_command(3, m, mode="arena"),
-    '!bo5_arena': lambda m: handle_bo_command(5, m, mode="arena"),
-    '!bo7_arena': lambda m: handle_bo_command(7, m, mode="arena"),
-    '!bo3_hcs': lambda m: handle_bo_command(3, m, mode="hcs"),
-    '!bo5_hcs': lambda m: handle_bo_command(5, m, mode="hcs"),
-    '!bo7_hcs': lambda m: handle_bo_command(7, m, mode="hcs"),
-    '!coinflip': lambda m: coinflip(),
-    '!number': lambda m: rand_number(),
-    '!botservers': lambda m: f"I'm in {len(client.guilds)} servers!"
+COMMAND_LOG_COUNT = {
+    'bo3_arena': 0, 'bo5_arena': 0, 'bo7_arena': 0,
+    'bo3_hcs': 0, 'bo5_hcs': 0, 'bo7_hcs': 0,
+    'bo3_legacy': 0, 'bo5_legacy': 0, 'bo7_legacy': 0,
+    'Coinflip': 0, 'Number': 0
 }
 
 class MatchCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-   
-    # arena Mode Commands
-    
+
     @discord.app_commands.command(name="bo3_arena", description="Starts a Bo3 series for Arena")
-    async def bo3_arena(self, interaction: discord.Interaction):
-        matches = series(3, mode="arena")
-        embed = create_embed(matches, 3, mode="arena")
-        await interaction.response.send_message(embed=embed)
+    async def bo3_arena(self, interaction):
+        matches = series(3, mode="Arena")
+        await interaction.response.send_message(embed=create_embed(matches, 3, "Arena"))
         COMMAND_LOG_COUNT['bo3_arena'] += 1
 
-    @discord.app_commands.command(name="bo5_arena", description="Starts a Bo5 series for Arena")
-    async def bo5_arena(self, interaction: discord.Interaction):
-        matches = series(5, mode="arena")
-        embed = create_embed(matches, 5, mode="arena")
-        await interaction.response.send_message(embed=embed)
-        COMMAND_LOG_COUNT['bo5_arena'] += 1
-
-    @discord.app_commands.command(name="bo7_arena", description="Starts a Bo7 series for Arena")
-    async def bo7_arena(self, interaction: discord.Interaction):
-        matches = series(7, mode="arena")
-        embed = create_embed(matches, 7, mode="arena")
-        await interaction.response.send_message(embed=embed)
-        COMMAND_LOG_COUNT['bo7_arena'] += 1
-    
-    # hcs Mode Commands
-    
-    @discord.app_commands.command(name="bo3_hcs", description="Starts a Bo3 series for HCS")
-    async def bo3_hcs(self, interaction: discord.Interaction):
-        matches = series(3, mode="hcs")
-        embed = create_embed(matches, 3, mode="hcs")
-        await interaction.response.send_message(embed=embed)
-        COMMAND_LOG_COUNT['bo3_hcs'] += 1
-
     @discord.app_commands.command(name="bo5_hcs", description="Starts a Bo5 series for HCS")
-    async def bo5_hcs(self, interaction: discord.Interaction):
-        matches = series(5, mode="hcs")
-        embed = create_embed(matches, 5, mode="hcs")
-        await interaction.response.send_message(embed=embed)
+    async def bo5_hcs(self, interaction):
+        matches = series(5, mode="HCS")
+        await interaction.response.send_message(embed=create_embed(matches, 5, "HCS"))
         COMMAND_LOG_COUNT['bo5_hcs'] += 1
 
-    @discord.app_commands.command(name="bo7_hcs", description="Starts a Bo7 series for HCS")
-    async def bo7_hcs(self, interaction: discord.Interaction):
-        matches = series(7, mode="hcs")
-        embed = create_embed(matches, 7, mode="hcs")
-        await interaction.response.send_message(embed=embed)
-        COMMAND_LOG_COUNT['bo7_hcs'] += 1
+    @discord.app_commands.command(name="bo7_legacy", description="Starts a Bo7 series for Legacy")
+    async def bo7_legacy(self, interaction):
+        matches = series(7, mode="Legacy")
+        await interaction.response.send_message(embed=create_embed(matches, 7, "Legacy"))
+        COMMAND_LOG_COUNT['bo7_legacy'] += 1
 
-     # Other Commands
-    
     @discord.app_commands.command(name="coinflip", description="Flips a coin")
-    async def coinflip(self, interaction: discord.Interaction):
-        result = coinflip()  # Assuming coinflip() is defined elsewhere
-        await interaction.response.send_message(result)
-        COMMAND_LOG_COUNT['Coinflip'] += 1
+    async def coinflip(self, interaction):
+        await interaction.response.send_message(coinflip())
 
     @discord.app_commands.command(name="number", description="Generates a random number")
-    async def number(self, interaction: discord.Interaction):
-        number = rand_number()  # Assuming rand_number() is defined elsewhere
-        await interaction.response.send_message(str(number))
-        COMMAND_LOG_COUNT['Number'] += 1
-
-    @discord.app_commands.command(name="botservers", description="Shows the number of servers the bot is in")
-    async def botservers(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f"I'm in {len(self.bot.guilds)} servers!")
-
+    async def number(self, interaction):
+        await interaction.response.send_message(rand_number())
 
 async def setup(bot):
     await bot.add_cog(MatchCommands(bot))
@@ -215,17 +158,6 @@ async def setup(bot):
 @bot.event
 async def on_ready():
     await setup(bot)
-    logging.info(f"We have logged in as {bot.user}")
-
-def checkTime():
-    # This function runs periodically every hour
-    threading.Timer(3600, checkTime).start()
-
-    # Log current time with format "Mon Month Day HH:MM:SS", e.g., "Thu Oct 14 15:30:45"
-    logging.info(f"Current Time = {datetime.now().strftime('%a %b %d %H:%M:%S')}")
-    logging.info(f"Command Log Count: {json.dumps(COMMAND_LOG_COUNT)}")
-
-
-checkTime()
+    logging.info(f"Logged in as {bot.user}")
 
 bot.run(token)
